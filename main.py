ceb24cf1131c9d6f8a433a8380d0341392417b3a
DB24-98 <Batterink.dick@gmail.com>
2022-09-07 15:37:20 +0200

    bosh

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ PORT = 30002         #host port
 
 
 def main():
-    _socket = buildsocket(HOST, PORT)   #Builidng socket
-    _socket.send(("Set_digital_out(0,True)"+"\n").encode('utf8'))
+    #_socket = buildsocket(HOST, PORT)   #Builidng socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #setting up socket
+    s.connect((HOST, PORT))   #conecting network socket
+    time.sleep(2)
+    s.send(("Set_digital_out(0,True)"+"\n").encode('utf8'))
 
 def buildsocket(ip, port):
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #setting up socket
     s.connect((ip, port))   #conecting network socket
     return s
