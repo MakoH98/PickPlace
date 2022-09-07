92c1872363dbdc5648678390072708dce377eb57
Marko <heldmarko@gmail.com>
2022-09-07 14:06:22 +0200

    code geschreven voor het remote verbinden aan de cobot

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import socket
 import time
 
+HOST = "192.168....."  # host adress
+PORT = "30002"         #host port
+
 
 def main():
-    print("tutorial voor het besturen van git")
-    print("tutorial voor het besturen van git 2")
-    Print("AWL pick and place")
+    buildsocket(HOST, PORT)
 
+
+def buildsocket(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
 
 
 
