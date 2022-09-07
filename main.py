2ab162a7f955981503a7a9576a6d5045538d2b7a
Marko <heldmarko@gmail.com>
2022-09-07 15:08:56 +0200

    added digital out control

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ PORT = "30002"         #host port
 
 
 def main():
-    buildsocket(HOST, PORT)   #Builidng socket
-
+    _socket = buildsocket(HOST, PORT)   #Builidng socket
+    _socket.send(("Set_digital_out(0,True)"+"\n").encode('utf8'))
 
 def buildsocket(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #setting up socket
     s.connect((ip, port))   #conecting network socket
-
+    return s
 
 
 if __name__ == '__main__':
