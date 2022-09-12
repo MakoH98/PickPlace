73c59387343c64ef4aac1797677900500df54101
Marko <heldmarko@gmail.com>
2022-09-12 12:18:42 +0200

    restructure en client probeersel

--- Backend.py
+++ Backend.py
@@ -1,9 +1,18 @@
 import socket
 
 
-def buildsocket(ip, port):
+def Hostsocket(ip, port):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #setting up socket in stream mode for Robot
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((ip, port))   #conecting network socket
     return s #returning network socket
+
+def Clientsocket(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(ip, port)
+    s.listen(5)
+    c, addr = s.accept()
+    msg = c.recv(1024)
+    return msg
--- main.py
+++ main.py
@@ -1,33 +1,73 @@
-from Backend import buildsocket
+from Backend import Hostsocket
+import socket
 import time
 
 
-HOST = "192.168.0.15"  # robot adress NB IP ADRESS FOR CLIENT NEEDS TO BE 192.168.0.XX
-PORT = 30002         #robot port for UR comms
-
+URIP = "192.168.0.15"  # robot adress NB IP ADRESS FOR CLIENT NEEDS TO BE 192.168.0.XX
+URPORT = 30002         #robot port for UR comms
+PCIP = "192.168.0.14"
+PCPORT = 30000
 
 def main():
-    _socket = buildsocket(HOST, PORT)   #Builidng socket
+    # pc_host(URIP, URPORT) sends commands via socket
+    UR_host(PCIP, PCPORT)
+
+def pc_host(HOST, PORT):
+    _socket = Hostsocket(HOST, PORT)   #Builidng socket
 
     #
     # time.sleep(1)
     # _socket.send(("set_digital_out(0,True)" + "\n").encode('utf8'))
     # time.sleep(2)
     # _socket.send(("set_digital_out(0,False)" + "\n").encode('utf8'))
-    while True:
+    count = 0
+    while count < 1:
         poses = [
-        "p[0.8,0.4,0.1,     3.14,0,0]", # x,y,z,rx,ry,rz
-        "p[0.8,0.4,0.8,     3.14,0,0]",  # x,y,z,rx,ry,rz
-        "p[0,1,0.8,     3.14,0,0]",  # x,y,z,rx,ry,rz
-        "p[0,1,0.1,     3.14,0,0]",  # x,y,z,rx,ry,rz
-        "p[0,1,0.8,     3.14,0,0]",  # x,y,z,rx,ry,rz
-        "p[0.8,0.4,0.8,     3.14,0,0]"  # x,y,z,rx,ry,rz
+        "p[0.6,0.6,0.6,     3.14,0,0]", # x,y,z,rx,ry,rz
+        "p[0.6,0.6,0.6,     1.58,0,0]",  # x,y,z,rx,ry,rz
+        "p[0,6,0.6,0.6,     3.14,0,0]",  # x,y,z,rx,ry,rz
+        "p[0.6,0.6,0.6,     -1.58,0,0]" # x,y,z,rx,ry,rz
+
         ]
+
         for pose in poses:
 
-            _socket.send((f"movel({pose}, a=0.5, v=1.3, t=0, r=0)" + "\n").encode('utf8'))
+            _socket.send((f"movej({pose}, a=0.5, v=0.2, t=0, r=0)" + "\n").encode('utf8'))
 
-            time.sleep(3)
+            time.sleep(5)
+
+        count = count + 1
+        data = _socket.recv(1024)
+        _socket.close()
+        print(f"Data from robot recieved...:{repr(data)}")
+
+
+
+def UR_host(HOST, PORT):
+    count = 0
+    while count < 1000:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        c, addr = s.accept()
+
+        try:
+            msg = c.recv(1024)
+            print(msg)
+
+            if msg == "Ask_Data":
+                count = count + 1
+                print("trying to send data..")
+                time.sleep(0.5)
+                command = "(200,50,45)"
+                c.send(command)
+
+        except socket.error as socketerror:
+            print(count)
+
+    c.close()
+    s.close()
 
 
 if __name__ == '__main__':
